9c26bf6683c782e7289d77363a54280861a5eafc
Adam Stone <146718294+Adam-Stone2@users.noreply.github.com>
2025-03-25 16:34:07 +0000

    Create Sample_Sigma_Squared.R
    Added the function to sample sigma Squared.

---
+++ R/Sample_Sigma_Squared.R
@@ -0,0 +1,7 @@
+Sample_Sigma_Squared<-function(yScaled,nu,lambda,SumOfAllTess){ #Sample sigma squared from inverse gamma distribution
+  
+  n=length(yScaled)
+  SigmaSquared<-rinvgamma(1,shape=(nu+n)/2,rate=(nu*lambda+sum((yScaled-SumOfAllTess)^2))/2)
+  
+  return(SigmaSquared)
+}
