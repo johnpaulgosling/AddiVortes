7d9b7e4214e4394506a95d42b520c7016750c479
Adam Stone <146718294+Adam-Stone2@users.noreply.github.com>
2025-03-25 16:41:34 +0000

    Create Cell_Indexes.R
    Added function to find cell index of an observation.

---
+++ Cell_Indexes.R
@@ -0,0 +1,9 @@
+Cell_Indexes<-function(x,Tess,Dim){ #Gives the row (the center) of the tessellation that each obseravtion falls within.
+  if (length(Tess[,1])==1){ #only 1 centre
+    CellsForGivenTess=rep(1,length(x[,1]))
+  }
+  else{ #multiple
+    CellsForGivenTess=knnx.index(Tess,matrix(x[,Dim],ncol = length(Dim)),1)
+  }
+  return(CellsForGivenTess)
+}
