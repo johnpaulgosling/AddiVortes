5a15603a7678cca9f118983196ead8cc4854546d
johnpaulgosling <johnpaulgosling@gmail.com>
2025-04-17 12:39:28 +0100

    Improving readability

--- R/AddiVortes.R
+++ R/AddiVortes.R
@@ -15,31 +15,36 @@
 #' @param sd The standard deviation.
 #' @param Omega The prior probability of adding a dimension.
 #' @param lambda_rate The rate of the Poisson distribution for the number of centers.
-#' @param YTest A vector of the output values for the test set.
-#' @param XTest A matrix of the covariates for the test set.
+#' @param yTest A vector of the output values for the test set.
+#' @param xTest A matrix of the covariates for the test set.
 #' @param IntialSigma The method used to calculate the initial sigma.
 #' @param thinning The thinning rate.
 #'
 #' @return The RMSE value for the test samples.
 #'
 #' @export
-AddiVortes <- function(y, x, m = 200, max_iter = 1200, burn_in = 200, nu = 6, q = 0.85, k = 3, sd = 0.8, Omega = 3, lambda_rate = 25, YTest, XTest, IntialSigma = "Linear", thinning = 1) {
+AddiVortes <- function(y, x, m = 200, max_iter = 1200, burn_in = 200, nu = 6, q = 0.85, k = 3, sd = 0.8, Omega = 3, lambda_rate = 25, yTest, xTest, IntialSigma = "Linear", thinning = 1) {
   # Scaling x and y
-  yScaled <- scale_data_internal(y)$scaled_data
-  x_scaling_result <- scale_data_internal(x)
-  xScaled <- x_scaling_result$scaled_data
-  train_centers <- x_scaling_result$centers # Vector of values
-  train_ranges <- x_scaling_result$ranges   # Vector of values
+  y_scaling_result <- scale_data_internal(y)
+  yScaled <- y_scaling_result$scaled_data # Vector of values
+  y_center <- y_scaling_result$centers
+  y_range <- y_scaling_result$ranges
 
-  XTest <- apply_scaling_internal(mat = XTest,
-                                   centers = train_centers,
-                                   ranges = train_ranges)
+  x_scaling_result <- scale_data_internal(x)
+  xScaled <- x_scaling_result$scaled_data # Matrix of values
+  x_centers <- x_scaling_result$centers # Vector of values
+  x_ranges <- x_scaling_result$ranges   # Vector of values
+
+  # Scaling the test set
+  xTest <- apply_scaling_internal(mat = xTest,
+                                  centers = x_centers,
+                                  ranges = x_ranges)
 
   # Initialise:
   # Prediction Set (A list of vectors with the output values for each tessellation),
-  # Dimension set (A list of vectors with the covariates included in the tessellaions);
-  # and Tessellation Set (A list of matrices that give the coordinates of the centers in the tessellations)
-
+  # Dimension set (A list of vectors with the covariates included in the tessellations);
+  # and Tessellation Set (A list of matrices that give the
+  #                          coordinates of the centres in the tessellations)
   Pred <- rep(list(matrix(mean(yScaled) / m)), m)
   Dim <- vector(length = m)
   Tess <- vector(length = m)
@@ -49,25 +54,32 @@ AddiVortes <- function(y, x, m = 200, max_iter = 1200, burn_in = 200, nu = 6, q 
   }
 
   # Prepare some variables used in the backfitting algorithm
-  SumOfAllTess <- rep(mean(yScaled), length(yScaled))
+  SumOfAllTess <- rep(mean(yScaled),
+                      length(yScaled))
   SigmaSquaredMu <- (0.5 / (k * sqrt(m)))^2
   LastTessPred <- matrix
 
-  # Matrices that will hold the samples from the poseterior distribution for the training samples and test samples.
+  # Matrices that will hold the samples from the posterior distribution
+  # for the training samples and test samples.
   posterior_samples <- floor((max_iter - burn_in) / thinning) + 1
-  PredictionMatrix <- array(dim = c(length(y), posterior_samples))
-  TestMatrix <- array(dim = c(length(YTest), posterior_samples))
+  PredictionMatrix <- array(dim = c(length(y),
+                                    posterior_samples))
+  TestMatrix <- array(dim = c(length(yTest),
+                              posterior_samples))
 
   # finding lambda
-  if (IntialSigma == "Naive") { # Usually used if p is greater then n. Uses Standard deviation of y to predict sigma.
+  if (IntialSigma == "Naive") {
+    # Usually used if p is greater then n. Uses Standard deviation of y to predict sigma.
     SigmaSquaredHat <- var(yScaled)
-  } else { # Default method using residual standard deviation from a least-squared linear regression of y, to predict sigma.
+  } else {
+    # Default method using residual standard deviation from a least-squared linear
+    # regression of y, to predict sigma.
     MultiLinear <- lm(yScaled ~ xScaled)
-    SigmaSquaredHat <- sum(MultiLinear$residuals^2) / (length(yScaled) - length(xScaled[1, ]) - 1)
+    SigmaSquaredHat <- sum(MultiLinear$residuals^2) /
+      (length(yScaled) - length(xScaled[1, ]) - 1)
   }
 
   # Find lambda
-  lambda <- 1
   lambda <- optim(
     par = 1,
     fitting_function,
@@ -77,67 +89,106 @@ AddiVortes <- function(y, x, m = 200, max_iter = 1200, burn_in = 200, nu = 6, q 
     q = q, nu = nu, sigmaSquared_hat = SigmaSquaredHat
   )$par
 
+  # Setting up progress bar
+  pbar <- utils::txtProgressBar(min = 0, max = max_iter,
+                                style = 3, width = 50, char = "=")
+
   for (i in 1:max_iter) {
     # Sample Sigma squared using all tessellations to predict the outcome variables
-    SigmaSquared <- Sample_Sigma_Squared(yScaled, nu, lambda, SumOfAllTess)
+    SigmaSquared <- Sample_Sigma_Squared(yScaled,
+                                         nu,
+                                         lambda,
+                                         SumOfAllTess)
 
     for (j in 1:m) {
-      NewTessOutput <- Propose_Tessellation(xScaled, j, Tess, Dim, sd) # Propose new Tessellation
+      # Propose new Tessellation
+      NewTessOutput <- Propose_Tessellation(xScaled,
+                                            j,
+                                            Tess,
+                                            Dim,
+                                            sd)
       TessStar <- NewTessOutput[[1]]
       DimStar <- NewTessOutput[[2]]
       Modification <- NewTessOutput[[3]]
 
-      ResidualsOutput <- Calculate_Residuals(yScaled, xScaled, j, SumOfAllTess, Tess, Dim, Pred, TessStar, DimStar, LastTessPred) # Calculate the n-vector of partial residuals derived from a fitting process that excludes the jth tessellation and the number of observations in each cell.
-      R_ijOld <- ResidualsOutput[[1]] # Old and New refer to the original and proposed tessellations
+      # Calculate the n-vector of partial residuals derived from a fitting process
+      # that excludes the jth tessellation and the number of observations in each cell.
+      ResidualsOutput <- Calculate_Residuals(yScaled, xScaled, j,
+                                             SumOfAllTess, Tess, Dim,
+                                             Pred, TessStar, DimStar,
+                                             LastTessPred)
+      # Old and New refer to the original and proposed tessellations
+      R_ijOld <- ResidualsOutput[[1]]
       n_ijOld <- ResidualsOutput[[2]]
       R_ijNew <- ResidualsOutput[[3]]
       n_ijNew <- ResidualsOutput[[4]]
-      SumOfAllTess <- ResidualsOutput[[5]] # Keeps track of the prediction for all tessellations to help sample sigma squared.
-      IndexesStar <- ResidualsOutput[[6]] # Gives the row of each observation for the cell it falls in for the proposed tessellation.
-      Indexes <- ResidualsOutput[[7]] # Gives the row of each observation for the cell it falls in for the original tessellation.
+      # Keeps track of the prediction for all tessellations to help sample sigma squared.
+      SumOfAllTess <- ResidualsOutput[[5]]
+      # Gives the row of each observation for the cell it falls in for the proposed tessellation.
+      IndexesStar <- ResidualsOutput[[6]]
+      # Gives the row of each observation for the cell it falls in for the original tessellation.
+      Indexes <- ResidualsOutput[[7]]
 
-      if (!any(n_ijNew == 0)) { # automatically rejects proposed tessellation if there exists a cell with no observations in.
+      if (!any(n_ijNew == 0)) {
+        # automatically rejects proposed tessellation if there exists a cell with no observations in.
+        LogAcceptanceProb <- Acceptance_Probability(xScaled, TessStar, DimStar,
+                                                    j, R_ijOld, n_ijOld, R_ijNew,
+                                                    n_ijNew, SigmaSquared,
+                                                    Modification, SigmaSquaredMu,
+                                                    Omega, lambda_rate)
 
-        LOGAcceptanceProb <- Acceptance_Probability(xScaled, TessStar, DimStar, j, R_ijOld, n_ijOld, R_ijNew, n_ijNew, SigmaSquared, Modification, SigmaSquaredMu, Omega, lambda_rate) # Gives the log of the acceptance probability.
-
-        if (log(runif(n = 1, min = 0, max = 1)) < LOGAcceptanceProb) { # Accepts the proposed tessellation is accepted then calculates the new output values for the new tessellation.
+        if (log(runif(n = 1)) < LogAcceptanceProb) {
+          # Accepts the proposed tessellation is accepted then calculates the new
+          # output values for the new tessellation.
           Tess <- TessStar
           Dim <- DimStar
-          Pred[[j]] <- Sample_mu_values(j, TessStar, R_ijNew, n_ijNew, SigmaSquaredMu, SigmaSquared)
+          Pred[[j]] <- Sample_mu_values(j, TessStar, R_ijNew, n_ijNew,
+                                        SigmaSquaredMu, SigmaSquared)
           LastTessPred <- Pred[[j]][IndexesStar]
-        } else { # Rejects the proposed tesellation then calculates new output values for the original tessellation.
-          Pred[[j]] <- Sample_mu_values(j, Tess, R_ijOld, n_ijOld, SigmaSquaredMu, SigmaSquared)
+        } else {
+          # Rejects the proposed tessellation then calculates new output values
+          # for the original tessellation.
+          Pred[[j]] <- Sample_mu_values(j, Tess, R_ijOld, n_ijOld,
+                                        SigmaSquaredMu, SigmaSquared)
           LastTessPred <- Pred[[j]][Indexes]
         }
-      } else { # Rejects the proposed tesellation then calculates new output values for the original tessellation.
-        Pred[[j]] <- Sample_mu_values(j, Tess, R_ijOld, n_ijOld, SigmaSquaredMu, SigmaSquared)
+      } else {
+        # Rejects the proposed tessellation then calculates new output values for
+        # the original tessellation.
+        Pred[[j]] <- Sample_mu_values(j, Tess, R_ijOld, n_ijOld,
+                                      SigmaSquaredMu, SigmaSquared)
         LastTessPred <- Pred[[j]][Indexes]
       }
-      if (j == m) { # If j equals m then adds the last tessellation output values to give a prediction.
+      if (j == m) {
+        # If j equals m then adds the last tessellation output values
+        # to give a prediction.
         SumOfAllTess <- SumOfAllTess + LastTessPred
       }
     }
 
-    if (i %% 100 == 0) {
-      cat(sprintf("Iteration %d out of %d", i, max_iter), "\n")
-    }
+    # Update the progress bar
+    utils::setTxtProgressBar(pbar, i)
 
-    if (i >= burn_in & (i - burn_in) %% thinning == 0) { # vectors that hold the predictions for each iteration after burn in.
+    if (i >= burn_in & (i - burn_in) %% thinning == 0) {
+      # vectors that hold the predictions for each iteration after burn in.
       PredictionMatrix[, 1 + (i - burn_in) / thinning] <- SumOfAllTess
-      TestMatrix[, 1 + (i - burn_in) / thinning] <- TestSet_Prediction(XTest, m, Tess, Dim, Pred)
+      TestMatrix[, 1 + (i - burn_in) / thinning] <- TestSet_Prediction(xTest, m, Tess, Dim, Pred)
     }
   }
 
-  # finding the mean of the predition over the iterations and then unscaling the predictions.
-  mean_yhat <- (rowSums(PredictionMatrix) / (posterior_samples)) *
-    (max(y) - min(y)) + ((max(y) + min(y)) / 2)
-  mean_yhat_Test <- (rowSums(TestMatrix) / (posterior_samples)) *
-    (max(y) - min(y)) + ((max(y) + min(y)) / 2)
+  # Close the progress bar
+  close(pbar)
+
+  # finding the mean of the prediction over the iterations and then unscaling the predictions.
+  mean_yhat <- (rowSums(PredictionMatrix) / (posterior_samples)) * y_range +
+    y_center
+  mean_yhat_Test <- (rowSums(TestMatrix) / (posterior_samples)) * y_range +
+    y_center
 
   return( # Returns the RMSE value for the test samples.
     data.frame(
       In_sample_RMSE = sqrt(mean((y - mean_yhat)^2)),
-      Out_of_sample_RMSE = sqrt(mean((YTest - mean_yhat_Test)^2))
+      Out_of_sample_RMSE = sqrt(mean((yTest - mean_yhat_Test)^2))
     )
   )
 }
