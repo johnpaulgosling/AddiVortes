d433822e6cc971c700d4e0ab63e1f0c9a8b834a4
johnpaulgosling <johnpaulgosling@gmail.com>
2025-05-16 16:45:38 +0100

    Refactor TestHelper to store results from AddiVortes and PredictAddiVortes, and update plotting

--- tests/TestHelper.R
+++ tests/TestHelper.R
@@ -14,11 +14,19 @@ TestSet <- 1:n
 TestSet <- TestSet[! TestSet %in% TrainSet]
 
 tic("Start")
-AddiVortes(Y_Boston[TrainSet],X_Boston[TrainSet,],
+results <- AddiVortes(Y_Boston[TrainSet],X_Boston[TrainSet,],
            200,2000,200,6,0.85,3,0.8,3,25,
            Y_Boston[TestSet],X_Boston[TestSet,],
            IntialSigma = "Linear")
+preds <- PredictAddiVortes(results[[1]],
+                           X_Boston[TestSet,],
+                           Y_Boston[TestSet])
 toc()
+
+results[[2]][[1]]
+preds[[1]]
+
+plot(Y_Boston[TestSet], preds[[2]])
 
 # Iteration 2000 out of 2000
 # In_sample_RMSE Out_of_sample_RMSE
