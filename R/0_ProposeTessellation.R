a39ae40dde53317476788c954ea56791708deb10
johnpaulgosling <johnpaulgosling@gmail.com>
2025-07-15 16:15:12 +0100

    Bump version to 0.1.17; update DESCRIPTION file and optimize proposeTessellation function for improved dimension handling and efficiency

--- R/0_ProposeTessellation.R
+++ R/0_ProposeTessellation.R
@@ -1,89 +1,67 @@
-#' @title Propose Tessellation
-#'
-#' @description Propose a new tessellation by adding, removing, changing or
-#' swapping dimensions or centres.
-#'
-#' @param x A matrix of covariates.
-#' @param j The index of the tessellation to be modified.
-#' @param Tess A list of matrices of the tessellations.
-#' @param Dim A list of vectors of the dimensions of the tessellations.
-#' @param var The variance of the normal distribution used to sample
-#' new coordinates.
-#' @param covariateIndices A sequence of integers representing the column indices.
-#' @param numCovariates The total number of covariates.
-#'
-#' @return A list containing the new tessellation, the new dimension matrix
-#' and the modification made.
-#' @keywords internal
-#' @noRd
-#'
 proposeTessellation <- function(tess_j, dim_j, var, covariateIndices, numCovariates) {
-  # This function now operates on a single tessellation matrix (tess_j)
-  # and dimension vector (dim_j), not the entire lists.
+  # This version optimises the selection of new dimensions to avoid
+  # creating intermediate vectors of "available covariates".
   
   p <- runif(1, 0, 1)
+  d_j_length <- length(dim_j)
+  tess_j_rows <- nrow(tess_j)
   
   dim_j_star <- dim_j
   tess_j_star <- tess_j
+  Modification <- "Change" # Default modification type
   
-  if (p < 0.2 && length(dim_j) != numCovariates ||
-      length(dim_j) == 1 && p < 0.4) {
-    # Add Dimension
-    available_covs <- covariateIndices[-dim_j]
-    if (length(available_covs) == 1) {
-      new_dim <- available_covs
-    } else {
-      new_dim <- sample(available_covs, 1)
+  if (p < 0.2 && d_j_length != numCovariates ||
+      d_j_length == 1 && p < 0.4) {
+    # Add Dimension: Use a more efficient sample-and-check loop
+    Modification <- "AD"
+    repeat {
+      new_dim <- sample.int(numCovariates, 1)
+      if (!any(dim_j == new_dim)) break
     }
     dim_j_star <- c(dim_j, new_dim)
-    tess_j_star <- cbind(tess_j, rnorm(nrow(tess_j), 0, var))
-    Modification <- "AD"
+    tess_j_star <- cbind(tess_j, rnorm(tess_j_rows, 0, var))
     
-  } else if (p < 0.4) {
+  } else if (p < 0.4 && d_j_length > 1) {
     # Remove Dimension
-    removed_dim_idx <- sample.int(length(dim_j), 1)
+    Modification <- "RD"
+    removed_dim_idx <- sample.int(d_j_length, 1)
     dim_j_star <- dim_j[-removed_dim_idx]
     tess_j_star <- tess_j[, -removed_dim_idx, drop = FALSE]
-    Modification <- "RD"
     
-  } else if (p < 0.6 || (p < 0.8 && nrow(tess_j) == 1)) {
+  } else if (p < 0.6 || (p < 0.8 && tess_j_rows == 1)) {
     # Add Centre
-    tess_j_star <- rbind(tess_j, rnorm(length(dim_j), 0, var))
     Modification <- "AC"
+    tess_j_star <- rbind(tess_j, rnorm(d_j_length, 0, var))
     
-  } else if (p < 0.8) {
+  } else if (p < 0.8 && tess_j_rows > 1) {
     # Remove Centre
-    if (nrow(tess_j) > 1) {
-      center_removed_idx <- sample.int(nrow(tess_j), 1)
-      tess_j_star <- tess_j[-center_removed_idx, , drop = FALSE]
-    }
-    # if only one centre, do nothing, effectively a "Change"
     Modification <- "RC"
+    center_removed_idx <- sample.int(tess_j_rows, 1)
+    tess_j_star <- tess_j[-center_removed_idx, , drop = FALSE]
     
-  } else if (p < 0.9 || length(dim_j) == numCovariates) {
-    # Change Centre
-    centre_to_change_idx <- sample.int(nrow(tess_j), 1)
-    tess_j_star[centre_to_change_idx, ] <- rnorm(length(dim_j), 0, var)
-    Modification <- "Change"
+  } else if (p < 0.9 || d_j_length == numCovariates) {
+    # Change Centre (This is the default modification)
+    centre_to_change_idx <- sample.int(tess_j_rows, 1)
+    tess_j_star[centre_to_change_idx, ] <- rnorm(d_j_length, 0, var)
     
   } else {
     # Swap Dimension
-    available_covs <- covariateIndices[-dim_j]
-    dim_to_change_idx <- sample.int(length(dim_j), 1)
-    
-    if (length(available_covs) == 1) {
-      new_dim <- available_covs
-    } else {
-      new_dim <- sample(available_covs, 1)
-    }
-    
-    dim_j_star[dim_to_change_idx] <- new_dim
-    tess_j_star[, dim_to_change_idx] <- rnorm(nrow(tess_j), 0, var)
     Modification <- "Swap"
+    # Select dimension to replace
+    dim_to_change_idx <- sample.int(d_j_length, 1)
+    # Select new dimension to replace it with
+    repeat {
+      new_dim <- sample.int(numCovariates, 1)
+      if (!any(dim_j == new_dim)) break
+    }
+    dim_j_star[dim_to_change_idx] <- new_dim
+    tess_j_star[, dim_to_change_idx] <- rnorm(tess_j_rows, 0, var)
   }
   
-  # Ensure the result is always a matrix, even with one row/column
-  tess_j_star <- matrix(tess_j_star, ncol = length(dim_j_star))
+  # Ensure the result is always a matrix
+  if (!is.matrix(tess_j_star)) {
+    tess_j_star <- matrix(tess_j_star, ncol = length(dim_j_star))
+  }
   
   return(list(tess_j_star, dim_j_star, Modification))
 }
